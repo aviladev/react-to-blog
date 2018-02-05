9bff61406e69d552a49bf56fe2cfbcd43d0e6b6c
Pedro Ávila <pedroavila95@gmail.com>
2018-02-04 23:32:40 -0200

    Add validation

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -2,20 +2,38 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
-  renderField = ({ input, label }) =>
+  renderField = ({ input, label, meta: {error, touched} }) => (
     <div className="form-group">
       <label htmlFor={label}>{label}</label>
       <input
-        className="form-control"
+        className={`form-control ${touched && error
+          ? 'is-invalid'
+          : touched 
+            ? 'is-valid'
+            : ''}`
+        }
         type="text"
         id={label}
         {...input}
       />
+      {touched
+        ? <div className="invalid-feedback">{error}</div>
+        : ''
+      }
     </div>
+  )
+
+  onSubmit = values => {
+    console.log(values)
+  }
 
   render () {
+    const { handleSubmit } = this.props
+    
     return (
-      <form className="container pt-2">
+      <form className="container pt-2"
+        onSubmit={ handleSubmit(this.onSubmit) }
+      >
         <Field
           label="Title"
           name="title"
@@ -31,11 +49,33 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     )
   }
 }
 
+const validate = values => {
+  const errors = {}
+
+  if (!values.title) {
+    errors.title = 'Enter a title!'
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter some categories'
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter some content'
+  }
+
+  return errors
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })( PostsNew )
